190e3a960f8ad15c0b920fd5c6bc918944c535e7
Steven Brien <sbrien@hlkagency.com>
2016-11-15 15:54:35 -0600

    Basic functions

--- templater/templater.py
+++ templater/templater.py
@@ -1,1 +1,35 @@
 # -*- coding: utf-8 -*-
+
+import os
+import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+from jinja2 import Template
+
+
+# Utility Functions
+# -------------------------------
+
+def template_factory(data, template_path):
+    def create_template(template_path):
+        f = open(template_path, 'r')
+        template_file = f.read()
+        f.close()
+        template = Template(template_file)
+        out = template.render(data=data)
+        return out
+    return create_template
+
+
+def open_json(json_file_path):
+    with open(json_file_path, 'r') as data:
+        return json.load(data)
+
+
+def get_json(json_url, params={}, username=None, password=None):
+    if username and password:
+        r = requests.get(json_url, params=params, auth=HTTPBasicAuth(username, password))
+        return r.json()
+    r = requests.get(json_url, params=params)
+    return r.json()
